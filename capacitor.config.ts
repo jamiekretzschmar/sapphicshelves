6efb48cea2ad6050b60518ae7452c84021132117
jamiekretzschmar <your-email@example.com>
2026-01-15 15:43:30 -0500

    Ready for Android build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sapphicshelves.app',
+  appName: 'SapphicShelves',
+  webDir: 'dist'
+};
+
+export default config;
