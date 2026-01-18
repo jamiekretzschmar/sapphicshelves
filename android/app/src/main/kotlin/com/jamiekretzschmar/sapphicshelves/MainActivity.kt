6b6662bc85813dc1d488227cf05b12e049b9e98a
Jamie Kretzschmar <jamie.kretzschamar@google.com>
2026-01-18 13:03:54 -0500

    Fix build errors and remove invalid resource files

--- android/app/src/main/kotlin/com/jamiekretzschmar/sapphicshelves/MainActivity.kt
+++ android/app/src/main/kotlin/com/jamiekretzschmar/sapphicshelves/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Ensure R is imported from the correct package if needed
         setContentView(R.layout.activity_main)
     }
 }
